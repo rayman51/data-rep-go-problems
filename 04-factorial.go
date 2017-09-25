1a027f1824c1a555cdca7552bf27491a7f55473f
rayman51 <ray.mannionit2@gmail.com>
2017-09-25 09:53:39 +0100

    added fact

--- 04-factorial.go
+++ 04-factorial.go
@@ -1,13 +1,18 @@
+//https://stackoverflow.com/questions/11270547/go-big-int-factorial-with-recursion
+
 package main
 
 import (
-	"fmt"
-	"math/big"
+	"fmt"      // imports fmt package
+	"math/big" // imports mathbig package
 )
 
 func main() {
+	factorial()
+} // main
+func factorial() {
 	x := new(big.Int)
 	x.MulRange(1, 100)
 	fmt.Println(x)
 
-}
+} // factorial
