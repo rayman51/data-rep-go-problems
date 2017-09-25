6087fa63d2c4a636191429b2b770ae00941872fe
rayman51 <ray.mannionit2@gmail.com>
2017-09-25 20:41:12 +0100

    sum function working

--- 04-factorial.go
+++ 04-factorial.go
@@ -1,18 +1,39 @@
-//https://stackoverflow.com/questions/11270547/go-big-int-factorial-with-recursion
-
+//https://stackoverflow.com/questions/46395819/get-sum-of-bigint-number-golang
 package main
 
 import (
-	"fmt"      // imports fmt package
-	"math/big" // imports mathbig package
+	"fmt"
+	"math/big"
 )
 
-func main() {
-	factorial()
-} // main
-func factorial() {
-	x := new(big.Int)
-	x.MulRange(1, 100)
-	fmt.Println(x)
+func factoral(n uint64) (r *big.Int) {
 
-} // factorial
+	one, bn := big.NewInt(1), new(big.Int).SetUint64(n)
+
+	r = big.NewInt(1)
+	if bn.Cmp(one) <= 0 {
+		return
+	}
+	for i := big.NewInt(2); i.Cmp(bn) <= 0; i.Add(i, one) {
+		r.Mul(r, i)
+	}
+	return
+}
+
+func add(number *big.Int) *big.Int {
+	ten := big.NewInt(10)
+	sum := big.NewInt(0)
+	mod := big.NewInt(0)
+	for ten.Cmp(number) < 0 {
+		sum.Add(sum, mod.Mod(number, ten))
+		number.Div(number, ten)
+	}
+	sum.Add(sum, number)
+	return sum
+}
+func main() {
+	fmt.Println("The factorial of 100 is ", factoral(100))
+
+	fmt.Println("The sum of factorial 100 is ", add(factoral(100)))
+
+}
