7196556c7c91f6cbf590a3aeb86c2fa784badc65
rayman51 <ray.mannionit2@gmail.com>
2017-10-01 00:52:26 +0100

    done

--- 01-hello-world.go
+++ 01-hello-world.go
@@ -1,12 +1,11 @@
 // https://gobyexample.com/hello-world
 
-package main
+package main // creates main package
 
-import "fmt"
+import "fmt" // imports package to allow print to screen
 
 func main() {
-	fmt.Println("Hello, world")
-	fmt.Println("こ ん に ち は 世 界")
-	
+	fmt.Println("Hello, world")  // prints to screen
+	fmt.Println("こ ん に ち は 世 界") // prints to screen
 
-}
+} // main
