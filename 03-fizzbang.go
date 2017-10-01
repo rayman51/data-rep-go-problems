d77e63cd8ac6d566bc47811424b995196dab10cb
rayman51 <ray.mannionit2@gmail.com>
2017-10-01 01:19:01 +0100

    done

--- 03-fizzbang.go
+++ 03-fizzbang.go
@@ -1,19 +1,20 @@
+//http://wiki.c2.com/?FizzBuzzTest
 package main
 
-import "fmt"
+import "fmt" // import package to print to screen
 
 func main() {
-    i := 1
-    for i <= 100 {
-        if (i % 3 == 0 && i % 5 == 0) {
-            fmt.Println("FizzBuzz")
-        } else if (i % 3 == 0) {
-            fmt.Println("Fizz")
-        } else if (i % 5 == 0) {
-            fmt.Println("Buzz")
-        } else {
-            fmt.Println(i)
-        }
-        i = i + 1
-    }
-}
+	i := 1         // initialises i at 1
+	for i <= 100 { // runs through a loop until i = 100
+		if i%3 == 0 && i%5 == 0 { //triggers if number is divisable by 3 & 5 with no remainder
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 { //triggers if number is divisable by 3 with no remainder
+			fmt.Println("Fizz")
+		} else if i%5 == 0 { //triggers if number is divisable 5 with no remainder
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i) //prints numbers to screen
+		} // if/else
+		i++ /// increments count
+	} // for
+} // main
