c988e853d6b49ae4f558cf5955869faaaa18d79a
rayman51 <ray.mannionit2@gmail.com>
2017-10-05 14:38:04 +0100

    got it workin

--- 04-factorial.go
+++ 04-factorial.go
@@ -2,62 +2,38 @@
 package main
 
 import (
-	"fmt"
-	"math/big"
+	"fmt"      // Import for input/output
+	"math/big" // Import for math
+	"strconv"  // import to convert string
 )
-/*func main() {
-	var num int
-	var fact int
-	fmt.Println("Enter the number to factorialise ...") // prompts user
-	fmt.Scanf("%d\n", &num)
 
-	if num == 0 {// if num = 0 prompts user to enter again 
+func digitSum(userinput int) int {
+
+	factorial := big.NewInt(1) 
+
+	// calculate factorial
+	for i := 1; i <= userinput; i++ {
+		factorial.Mul(factorial, big.NewInt(int64(i))) 
+	}
+	fmt.Println(userinput,"!  : ",factorial)//output factorial
+
+	var factSum, digit int  
+
+	//calculate sum of factorial digits
+	for i := range factorial.String() {
+			digit, _ = strconv.Atoi(string(factorial.String()[i])) 
+			factSum += digit                                 
+	}
+	return factSum
 	
-		fmt.Println("   Invalid Entry Please Enter again : ")
-		fmt.Scanf("%d", &num);
-		fmt.Println("====================================\n ")
-	}// if
+}//digitSum
 
-	if num>0 {// if valid number run the while loop 
-	
-		// loop to multiply numbers
-		for num > 0{
-			fact *= num//multiplies number 
-			num--// decrements number
-		}// while
-	}// if
-	fmt.Printf("    Factorial is : %d", fact)
-
-} // main*/
-
-func factoral(n uint64) (r *big.Int) {
-
-	one, bn := big.NewInt(1), new(big.Int).SetUint64(n)
-
-	r = big.NewInt(1)
-	if bn.Cmp(one) <= 0 {
-		return
-	}
-	for i := big.NewInt(2); i.Cmp(bn) <= 0; i.Add(i, one) {
-		r.Mul(r, i)
-	}
-	return
-}
-
-func add(number *big.Int) *big.Int {
-	ten := big.NewInt(10)
-	sum := big.NewInt(0)
-	mod := big.NewInt(0)
-	for ten.Cmp(number) < 0 {
-		sum.Add(sum, mod.Mod(number, ten))
-		number.Div(number, ten)
-	}
-	sum.Add(sum, number)
-	return sum
-}
 func main() {
-	fmt.Println("The factorial of 100 is ", factoral(100))
+	//take user input from console
+	var userinput int
+	fmt.Print("Please enter a number(1-100): " )
+	fmt.Scan(&userinput)
 
-	fmt.Println("The sum of factorial 100 is ", add(factoral(100)))
-
+	fmt.Println("Input : ",userinput)
+	fmt.Println("Sum of Factorial Digits : ",digitSum(userinput))
 }
